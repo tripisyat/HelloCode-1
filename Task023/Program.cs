ffaf8c1e837082a92c55b53c5b0735709961f1b1
Dmitry Lopatinsky <Lopatinskyda@gmail.com>
2021-12-27 22:00:41 +0400

    добавил задачу показать таблицу квадратов чисел от 1 до N

---
+++ Task023/Program.cs
@@ -0,0 +1,13 @@
+﻿//показать таблицу квадратов чисел от 1 до N
+
+Console.WriteLine("Введите число N");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i*i <= N; i++)
+{
+    System.Console.Write($"[{i}] = {i*i},\t");// \t - табуляция
+    if (i % 5 == 0)
+    {
+        Console.WriteLine();
+    }
+}
